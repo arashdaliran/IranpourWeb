a038e96b0e2a48ecb48e918c14211367cafda0b4
Arash Daliran <arashdaliran@gmail.com>
2022-11-30 17:51:10 +0330

    fixed home paddings

--- src/components/LatestComponent.vue
+++ src/components/LatestComponent.vue
@@ -9,16 +9,16 @@ const vp = useViewport()
         <h2 :class="{ biggerFont: !vp.isMobile }">Latest Singles</h2>
         <a :class="{ albumDesktop: !vp.isMobile }" href="/musics#caucasian">
             <img class="albumImage" src="../assets/images/album/caucasian.jpg" />
-            <h3 class="marginWithFoto">Caucasian</h3>
-            <h4 class="marginWithFoto">2022</h4>
-            <p class="marginWithFoto">This piece contains the local-folk atmosphere of Caucasian music. After listening
+            <h3>Caucasian</h3>
+            <h4>2022</h4>
+            <p class="collapseText">This piece contains the local-folk atmosphere of Caucasian music. After listening
                 to this piece, the local celebrations and the pristine nature of this area can be imagined. </p>
         </a>
         <a :class="{ albumDesktop: !vp.isMobile }" href="/musics#hengam">
             <img class="albumImage" src="../assets/images/album/Hengam.jpeg" />
-            <h3 class="marginWithFoto">Hengam</h3>
-            <h4 class="marginWithFoto">2020</h4>
-            <p class="marginWithFoto">Inspired by the red sand beach called Hormuz, and the calmness of
+            <h3>Hengam</h3>
+            <h4>2020</h4>
+            <p class="collapseText">Inspired by the red sand beach called Hormuz, and the calmness of
                 Hengam Island, which are located in the south of Iran. This piece consists of two parts, the first part
                 is the prelude. </p>
         </a>
@@ -38,13 +38,19 @@ h2 {
     margin: 30px;
 }
 
-.marginWithFoto {
-    padding-top: 10px;
-    display: block;
-    width: 75%;
-    margin: auto;
+a {
+    display: inline-block;
+    padding: 30px 0;
+    h3,
+    h4,
+    p {
+        padding-top: 10px;
+        width: 85%;
+        margin: auto;
+    }
+}
 
-    //adding collapse to text
+.collapseText {
     display: -webkit-box;
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
@@ -62,19 +68,18 @@ h2 {
 .albumDesktop {
     padding: 10px;
     margin-right: 0;
-    display: inline-block;
 
-    >img {
+    img {
         padding-left: 20px;
+        padding-right: 50px;
         width: 40%;
         float: left;
     }
 
-    >h3,
+    h3,
     h4,
     p {
-        padding-left: 10vmin;
-        padding-bottom: 20px;
+        padding-bottom: 10px;
         text-align: left;
         width: auto;
     }
