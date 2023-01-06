f06dce6491579037aa835ab2cb93b7de893a44d2
Arash Daliran <arashdaliran@gmail.com>
2023-01-06 15:12:52 +0330

    added gallery image preview

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,22 +1,56 @@
 <script setup>
 import NavigationBar from '../components/NavigationBar.vue';
+import { ref,computed } from 'vue';
 import { useViewport } from '../plugins/NoobiesQueries';
+import { useUrl } from '../plugins/NoobiesUrlMaker';
+const images = [
+    {
+        address : "../assets/images/IMG_20200606_125256_869.jpg",
+        pos : "0% 30%"
+    },
+    {
+        address : "../assets/images/IMG_20220722_132350_403.jpg",
+        pos : "0% 30%"
+    },
+    {
+        address : "../assets/images/IMG_20220722_132355_963.jpg",
+        pos : "0% 10%"
+    },  
+    {
+        address : "../assets/images/Iranpour.jpeg",
+        pos : "0% 30%"
+    },
+    
+]
 
 const vp = useViewport()
-
+const showPreview = ref(false)
+const previewSource = ref("")
+function openPreview(address) {
+    previewSource.value = useUrl(address)
+    showPreview.value = true
+}
 </script>
 <template>
     <NavigationBar class="mobileNav"/>
     <main class="main">
         <h1>Gallery</h1>
-        <div class="responsive"  :class="{tabletGrid : vp.isTablet, desktopGrid : vp.isDesktop }" >
-            <img src="../assets/images/IMG_20200606_125256_869.jpg" style="object-position: 0% 30%;" />
-            <img src="../assets/images/IMG_20220722_132350_403.jpg" style="object-position: 0% 30%;" />
-            <img src="../assets/images/IMG_20220722_132355_963.jpg" style="object-position: 0% 10%;"/>
-            <img src="../assets/images/Iranpour.jpeg" style="object-position: 0% 30%;" />
-
+        <div class="gridGallery"  :class="{tabletGrid : vp.isTablet, desktopGrid : vp.isDesktop }" >
+            <button v-for="img in images" @click="openPreview(img.address)">
+                <img :src="useUrl(img.address)" :style="{objectPosition : img.pos}" />
+            </button>
         </div>
     </main>
+    <div v-if="showPreview" class="imagePreviewParent">
+        <div class="div">
+            <button class="closeBtn" @click="showPreview = false">
+                <img src="../assets/images/close.svg" />
+            </button>
+            <div class="imageBox">
+                <img class="prevImage" :src="previewSource">
+            </div>
+        </div>
+    </div>
 
 </template>
 <style scoped lang="scss">
@@ -32,18 +66,25 @@ const vp = useViewport()
 h1 {
     text-align: center;
 }
+button:hover{
+    background-color: transparent;
+    border-radius: 0;
+}
 
 .main {
     margin-top: $appBarHeight;
 }
+img{
+    width: 100%;
+    height: 100%;
+}
 
-.responsive {
+.gridGallery {
     margin-top: 20px;
     display: grid;
     grid-template-columns: auto;
     gap: 10px;
-    img {
-        width: 100%;
+    button {
         height: 350px;
     }
 }
@@ -53,5 +94,49 @@ h1 {
 }
 .desktopGrid{
     grid-template-columns: auto auto auto;
+    button {
+        height: 400px;
+    }
+}
+.imagePreviewParent{
+    position: fixed;
+    z-index: 150;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.814);
+    .div{
+        display: block;
+        margin: auto;
+        margin-top: $appBarHeight ;
+        width: 95%;
+        height: 100%;
+    }
+}
+.closeBtn{
+    margin-top: 10px;
+    display: block;
+    position: absolute;
+    top: $appBarHeight + 10px;
+    right: 10px;
+    background-color: rgba(0, 0, 0, 0.50);
+    border-radius: 10px;
+}
+.closeBtn:hover{
+    background-color:rgba(37, 35, 32, 0.8) ;
+    border-radius: 10px;
+}
+.imageBox{
+    display: block;
+    position: static;
+    width: 98%;
+    height: 89%;
+    margin: auto;
+}
+.prevImage{
+    display: block;
+    object-fit: contain;
+    margin: auto;
 }
 </style>
